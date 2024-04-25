8a478d6537258e15c1bf1a644079b6c4f8e3ef98
deep-69bits <lcb2021004@iiitl.ac.in>
2024-04-26 03:42:34 +0530

    chaneges

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Layout children={undefined}>{children}</Layout>
+        <Layout >{children}</Layout>
       </body>
     </html>
   );
