f02890aab218e865bf7e3c5785258b6d8e2a8e88
deep-69bits <lcb2021004@iiitl.ac.in>
2024-04-30 04:44:03 +0530

    changes

--- src/app/(dashboard)/dashboard/profile/page.tsx
+++ src/app/(dashboard)/dashboard/profile/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 import React, { use, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { supabase } from "@/lib/SupabaseClient";
 
 const Page = () => {
-
- 
-  const [description, setDescription] = useState("");
-  const [coverPhoto, setCoverPhoto] = useState<any>(null);
+  const [about, setAbout] = useState("");
   const [state, setState] = useState("");
   const [city, setCity] = useState("");
   const [streetAddress, setStreetAddress] = useState("");
@@ -15,16 +13,77 @@ const Page = () => {
   const [thumbnail, setThumbnail] = useState<any>(null);
   const [imageProfile, setImageProfile] = useState<any>(null);
   const [user, setUser] = useState<any>(null);
-  const [category, setCategory] = useState<any>("Bike");
-  const [phoneNumber, setPhoneNumber] = useState<any>(null);
-  const [name, setName] = useState("");
-  const [used, setUsed] = useState("");
   const router = useRouter();
 
+   useEffect(() => {
+     const getUser = async () => {
+       const {
+         data: { user },
+       } = await supabase.auth.getUser();
+       if (user) {
+        console.log(user)
+         setUser(user);
+       } else {
+         router.push("/");
+       }
+     };
+     getUser();
+   }, []);
+
+  const handleImageChange = (e: any) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageProfile(file);
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const thumbnail = e.target.result;
+        setThumbnail(thumbnail);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  function generated(length: number) {
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+
+  const handleSubmit=async(e:any)=>{
+     e.preventDefault()
+     const imageName = imageProfile.name + generated(6);
+     console.log(imageName);
+     var { data, error } = await supabase.storage
+       .from("product_images")
+       .upload("/" + imageName, imageProfile, {
+         cacheControl: "3600",
+         upsert: false,
+       });
+       console.log({data,error})
+     const imageurl =
+       "https://nllszuxcqbnhgngchcau.supabase.co/storage/v1/object/public/product_images/" +
+       imageName;
+        console.log(user.id)
+        const response = await supabase.from("users").update({
+          profile_pic: imageurl,
+          street_address: streetAddress,
+          city: city,
+          state: state,
+          postal_code: postalCode,
+          about: about,
+        }).eq('id',user.id);
+        router.replace('/dashboard')
+  }
 
   return (
     <div className="py-10">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="space-y-12">
           <div className="border-b border-gray-900/10 pb-12">
             <h2 className="text-base font-semibold leading-7 text-gray-900">
@@ -48,6 +107,9 @@ const Page = () => {
                     id="about"
                     name="about"
                     rows={3}
+                    onChange={(e: any) => {
+                      setAbout(e.target.value);
+                    }}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   ></textarea>
                 </div>
@@ -65,18 +127,28 @@ const Page = () => {
                 </label>
                 <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                   <div className="text-center">
-                    <svg
-                      className="mx-auto h-12 w-12 text-gray-300"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
+                    {thumbnail ? (
+                      <>
+                        <img
+                          src={thumbnail}
+                          alt="Preview"
+                          className=" object-cover mb-4"
+                        />
+                      </>
+                    ) : (
+                      <svg
+                        className="mx-auto h-12 w-12 text-gray-300"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    )}
                     <div className="mt-4 flex text-sm leading-6 text-gray-600">
                       <label
                         htmlFor="file-upload"
@@ -87,6 +159,7 @@ const Page = () => {
                           id="file-upload"
                           name="file-upload"
                           type="file"
+                          onChange={handleImageChange}
                           className="sr-only"
                         />
                       </label>
@@ -120,6 +193,9 @@ const Page = () => {
                 <div className="mt-2">
                   <input
                     type="text"
+                    onChange={(e: any) => {
+                      setStreetAddress(e.target.value);
+                    }}
                     name="street-address"
                     id="street-address"
                     autoComplete="street-address"
@@ -138,6 +214,9 @@ const Page = () => {
                 <div className="mt-2">
                   <input
                     type="text"
+                    onChange={(e: any) => {
+                      setCity(e.target.value);
+                    }}
                     name="city"
                     id="city"
                     autoComplete="address-level2"
@@ -156,6 +235,9 @@ const Page = () => {
                 <div className="mt-2">
                   <input
                     type="text"
+                    onChange={(e: any) => {
+                      setState(e.target.value);
+                    }}
                     name="region"
                     id="region"
                     autoComplete="address-level1"
@@ -175,6 +257,9 @@ const Page = () => {
                   <input
                     type="text"
                     name="postal-code"
+                    onChange={(e: any) => {
+                      setPostalCode(e.target.value);
+                    }}
                     id="postal-code"
                     autoComplete="postal-code"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
